c55da17c5ceb75d1f08651c098bd63da78299b0f
SerjBaranov <serj.viber@gmail.com>
2023-05-25 17:13:09 +0300

    modified: FolderEvents.py: string -> f-string

--- FolderEvents.py
+++ FolderEvents.py
@@ -13,21 +13,21 @@ class Handler(FileSystemEventHandler):
         return
 
     def on_created(self, event):
-        self.strOutput = 'Event '+ event.event_type + ' file '+event.src_path + '\n'
+        self.strOutput = f'Event {event.event_type} file {event.src_path} \n'
         with open(self.fileslog, 'a', encoding="utf-8") as e:
             e.writelines(self.strOutput)
             e.close()
         return
 
     def on_deleted(self, event1):
-        self.strOutput = 'Event '+ event1.event_type + ' file '+event1.src_path + '\n'
+        self.strOutput = f'Event {event1.event_type} file {event1.src_path} \n'
         with open(self.fileslog, 'a', encoding="utf-8") as g:
             g.writelines(self.strOutput)
             g.close()
         return
 
     def on_moved(self, event2):
-        self.strOutput = 'Event '+ event2.event_type + ' file '+event2.src_path + ' where '+ event2.dest_path + '\n'
+        self.strOutput = f'Event {event2.event_type} file {event2.src_path} where {event2.dest_path} \n'
         with open(self.fileslog, 'a', encoding="utf-8") as h:
             h.writelines(self.strOutput)
             h.close()
